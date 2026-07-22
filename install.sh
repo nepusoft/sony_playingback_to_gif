463abf5ba233c0eab17d8861b0e120aa79590b6b
neptusoft <Eli-Logiciels@outlook.fr>
2026-07-22 22:12:14 +0200

    add script to install on linux and macos

---
+++ install.sh
@@ -0,0 +1,9 @@
+curl -LsSf https://astral.sh/uv/install.sh | sh
+uv python install 3.15.0b2
+cd /tmp
+pip3 install pillow
+pip3 install opencv-python
+pip3 install pathlib
+wget https://github.com/nepusoft/sony_playingback_to_gif/raw/refs/heads/main/install.py
+python3 install.py
+rm install.py
